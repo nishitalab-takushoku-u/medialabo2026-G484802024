be7675a68ac858d4e5f63af64586d649e3a07700
G484802024 <248480@st.takushoku-u.ac.jp>
2026-06-18 21:21:32 +0900

    elements.html

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,11 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+console.log(campus.address);
 
+for (let room of campus.buildingD) {
+    console.log(room);
+}
+for (let g of gakka) {
+    console.log(g.name); 
+}
