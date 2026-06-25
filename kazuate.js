177aa691d813ae7e2f6df9143d6e888baec68e63
G484802024 <248480@st.takushoku-u.ac.jp>
2026-06-25 17:17:03 +0900

    kazuate.js

--- kazuate.js
+++ kazuate.js
@@ -12,19 +12,42 @@ let kaisu = 0;
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // ここから: 予想回数を1増やして，span#kaisu 要素のテキストを更新
-
+  kaisu = kaisu + 1;
+  let spanKaisu = document.querySelector('#kaisu');
+  
+  if (kaisu <= 3) {
+    spanKaisu.textContent = kaisu;
+  }
   // ここまで: 予想回数を1増やして，span#kaisu 要素のテキストを更新
   
   // ここから: テキストボックスに指定された数値を yoso に代入する
   let yoso;
+  let inputYosou = document.querySelector('#yosou'); 
+  yoso = Number(inputYosou.value);
   // ここまで: テキストボックスに指定された数値を yoso に代入する
   
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
+let pResult = document.querySelector('#result');
 
+  if (kaisu >= 4) {
+    pResult.textContent = '答えは ' + kotae + ' でした．すでにゲームは終わっています';
+  } else if (yoso === kotae) {
+    pResult.textContent = '正解です．おめでとう!';
+    kaisu = 4; 
+  } else {
+    if (kaisu === 3) {
+      pResult.textContent = 'まちがい．残念でした答えは ' + kotae + ' です．';
+    } else if (yoso < kotae) {
+      pResult.textContent = 'まちがい．答えはもっと大きいですよ';
+    } else if (yoso > kotae) {
+      pResult.textContent = 'まちがい．答えはもっと小さいですよ';
+    }
+  }
   // ここまで: 正解判定する
 }
 
 // ここから: ボタンを押した時のイベントハンドラとして hantei を登録
-
+let btn = document.querySelector('#answer_btn');
+btn.addEventListener('click', hantei);
 // ここまで: ボタンを押した時のイベントハンドラとして hantei を登録
