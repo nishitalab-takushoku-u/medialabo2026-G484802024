ffdb34f0c98bf43cc9b35681d3d4217251c187a9
G484802024 <248480@st.takushoku-u.ac.jp>
2026-07-16 17:13:46 +0900

    tv-program.js

--- tv-program.js
+++ tv-program.js
@@ -1,12 +1,12 @@
 function print(data) {
-    for (let program of data.list.g1) {
+    for (var program of data.list.g1) {
         console.log('--- 番組情報 ---');
-        console.log('開始時刻: ' + program.start_time);
-        console.log('終了時刻: ' + program.end_time);
+        console.log('番組開始時刻: ' + program.start_time);
+        console.log('番組終了時刻: ' + program.end_time);
         console.log('チャンネル: ' + program.service.name);
         console.log('番組名: ' + program.title);
-        console.log('サブタイトル: ' + program.subtitle);
-        console.log('説明文: ' + program.content);
+        console.log('番組サブタイトル: ' + program.subtitle);
+        console.log('番組説明文: ' + program.content);
         console.log('出演者: ' + program.act);
     }
 }
@@ -14,6 +14,11 @@ function print(data) {
 function printDom(data) {
     var resultDiv = document.querySelector('div#result');
     resultDiv.innerHTML = '';
+
+    if (!data || !data.list) {
+        resultDiv.innerHTML = '<p>番組データが見つかりませんでした。</p>';
+        return;
+    }
 
     var table = document.createElement('table');
 
@@ -31,28 +36,59 @@ function printDom(data) {
 
     var bodyHtml = '';
 
-    for (var program of data.list.g1) {
-        bodyHtml = bodyHtml + '<tr>' +
-                   '<td>' + program.start_time + '</td>' +
-                   '<td>' + program.end_time + '</td>' +
-                   '<td>' + program.service.name + '</td>' +
-                   '<td>' + program.title + '</td>' +
-                   '<td>' + program.subtitle + '</td>' +
-                   '<td>' + program.content + '</td>' +
-                   '<td>' + program.act + '</td>' +
-                   '</tr>';
+    var serviceSelect = document.querySelector('select#service');
+    var selectedService = serviceSelect.value;
+    var programList = data.list[selectedService];
+
+    if (programList) {
+        for (var program of programList) {
+            bodyHtml = bodyHtml + '<tr>' +
+                       '<td>' + program.start_time + '</td>' +
+                       '<td>' + program.end_time + '</td>' +
+                       '<td>' + program.service.name + '</td>' +
+                       '<td>' + program.title + '</td>' +
+                       '<td>' + program.subtitle + '</td>' +
+                       '<td>' + program.content + '</td>' +
+                       '<td>' + program.act + '</td>' +
+                       '</tr>';
+        }
     }
 
     table.innerHTML = headerHtml + '<tbody>' + bodyHtml + '</tbody>';
     resultDiv.insertAdjacentElement('beforeend', table);
 }
 
-function sendRequest() {
+var btn = document.querySelector('#submitButton');
+btn.addEventListener('click', sendRequest);
 
+function sendRequest() {
+    var serviceSelect = document.querySelector('select#service');
+    var genreSelect = document.querySelector('select#genre');
+    
+    var serviceValue = serviceSelect.value;
+    var genreValue = genreSelect.value;
+
+    var url = 'https://www.nishita-lab.org/web-contents/jsons/nhk/' + serviceValue + '-' + genreValue + '-j.json';
+
+    axios.get(url)
+        .then(showResult)
+        .catch(showError)
+        .then(finish);
 }
 
 function showResult(resp) {
+    var data = resp.data;
 
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    if (data === null || data === undefined) {
+        console.log('データが空です');
+        return;
+    }
+
+    printDom(data);
 }
 
 function showError(err) {
@@ -62,84 +98,3 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ Ｃｉｔｙ Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは… テーマ曲 ♪「Ｏｎ Ｙｏｕｒ Ｍａｒｋ」はＡｗｅｓｏｍｅ Ｃｉｔｙ Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ Ｃｉｔｙ Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
-  }
-};
